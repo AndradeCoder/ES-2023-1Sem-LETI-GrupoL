8fe0de20e4d82367a1491aeb67c57607503fb3ee
AndradeCoder <leothebest2002@hotmail.com>
2023-11-04 15:56:06 +0000

    Sync do projeto com o repositório
    Este é essencialmente o commit inicial do projeto, onde se apenas conectou o projeto no eclipse ao repositório.
    Adicionou-se já a dependência/biblioteca do CSV do Apache Commons

---
+++ AppGestaoHorarios/src/main/java/es_grupoL/AppGestaoHorarios/App.java
@@ -0,0 +1,13 @@
+package es_grupoL.AppGestaoHorarios;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
