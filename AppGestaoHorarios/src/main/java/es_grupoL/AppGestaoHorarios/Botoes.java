0107ad6e3413968078b51d2cb47f59439859151d
AndradeCoder <leothebest2002@hotmail.com>
2023-11-05 06:56:44 +0000

    Novos updates no Botoes.java
    Mudanças de formatação, nomenclatura e confirmação do tipo de ficheiro selecionado

--- AppGestaoHorarios/src/main/java/es_grupoL/AppGestaoHorarios/Botoes.java
+++ AppGestaoHorarios/src/main/java/es_grupoL/AppGestaoHorarios/Botoes.java
@@ -10,43 +10,50 @@ import java.io.File;
 import javax.swing.JFileChooser;
 
 public class Botoes extends JFrame {
-    public Botoes() {
-    	setLayout(new FlowLayout());
-    	
-        JButton botaocf = new JButton("Carregar ficheiro");
-        JButton botaoaw = new JButton("Abre Website");
 
-        botaocf.setPreferredSize(new Dimension(200, 100));
-        botaocf.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	JFileChooser fc = new JFileChooser();
-                int f = fc.showOpenDialog(null);
-                if (f == JFileChooser.APPROVE_OPTION) {
-                    File fs = fc.getSelectedFile();
-                    System.out.println("Ficheiro selecionado: " + fs.getAbsolutePath());
-                }
-            }
-        }
-        );
-        botaoaw.setPreferredSize(new Dimension(200, 100));
-        botaoaw.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("funciona");
-            }
-        }
-        );
+	public Botoes() {
+		setLayout(new FlowLayout());
 
-        add(botaocf);
-        add(botaoaw);
+		JButton fileButton = new JButton("Carregar ficheiro");
+		JButton websiteButton = new JButton("Abre Website");
 
-        setTitle("JFrame");
-        setSize(500, 150);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
+		fileButton.setPreferredSize(new Dimension(200, 100));
+		fileButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+				int f = fc.showOpenDialog(null);
+				if (f == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = fc.getSelectedFile();
+					String filePath = selectedFile.getAbsolutePath();
+					System.out.println(filePath);
+					if (filePath.toLowerCase().endsWith(".csv")) {
+						System.out.println("Ficheiro CSV selecionado: " + filePath);
+					} else {
+						System.out.println("Por favor selecione um ficheiro .csv");
+					}
+				}
+			}
+		});
 
-    public static void main(String[] args) {
-        new Botoes();
-    }
+		websiteButton.setPreferredSize(new Dimension(200, 100));
+		websiteButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("funciona");
+			}
+		});
+
+		add(fileButton);
+		add(websiteButton);
+
+		setTitle("JFrame");
+		setSize(500, 150);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+//    public static void main(String[] args) {
+//        new Botoes();
+//    }
 }
-
