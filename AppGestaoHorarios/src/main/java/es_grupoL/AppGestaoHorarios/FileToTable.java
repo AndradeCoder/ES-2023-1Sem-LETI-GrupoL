5a4a6040931b62f8d4b1933202084bd47c1b526f
lpfaoiscte <82105501+fenixoide@users.noreply.github.com>
2023-11-22 17:03:52 +0000

    apenas completa a lista de campos

--- AppGestaoHorarios/src/main/java/es_grupoL/AppGestaoHorarios/FileToTable.java
+++ AppGestaoHorarios/src/main/java/es_grupoL/AppGestaoHorarios/FileToTable.java
@@ -23,20 +23,20 @@ public class FileToTable {
 	private List<String> mappedHeader = new ArrayList<>();
 
 	/**
-	 * Constructs a `FileToTable` object for processing a given CSV file.
-	 *
-	 * @param file The CSV file to be processed.
-	 */
+     * Constructs a `FileToTable` object for processing a given CSV file.
+     *
+     * @param file The CSV file to be processed.
+     */
 	public FileToTable(File file) {
 		this.file = file;
 	}
 
+	
 	/**
-	 * Reads the CSV file and converts it into a `Map` containing rows of data.
-	 *
-	 * @return A `Map` where the key is the row number and the value is a list of
-	 *         column values.
-	 */
+     * Reads the CSV file and converts it into a `Map` containing rows of data.
+     *
+     * @return A `Map` where the key is the row number and the value is a list of column values.
+     */
 	public Map<Integer, ArrayList<String>> readCSV() {
 		Map<Integer, ArrayList<String>> fileLineInfo = new HashMap<>();
 
@@ -57,13 +57,12 @@ public class FileToTable {
 	}
 
 	/**
-	 * Creates an HTML representation of the data for visualization using
-	 * JavaScript.
-	 *
-	 * @param data A `Map` containing data rows to be converted into an HTML table.
-	 */
+     * Creates an HTML representation of the data for visualization using JavaScript.
+     *
+     * @param data A `Map` containing data rows to be converted into an HTML table.
+     */
 	public void createHTML(Map<Integer, ArrayList<String>> data) {
-		System.out.println("VER ISTO " + mappedHeader);
+		System.out.println("VER ISTO "+mappedHeader);
 		StringBuilder jsCode = new StringBuilder();
 		jsCode.append("<script type=\"text/javascript\">\n\n");
 		jsCode.append("var tabledata = [");
@@ -73,15 +72,12 @@ public class FileToTable {
 			List<String> rowData = entry.getValue();
 			int column = 0;
 
-			jsCode.append("{");
+			jsCode.append("{");  
 			for (String fieldName : this.mappedHeader) {
 				if (column == rowData.size() - 1)
-					jsCode.append(ColunasHorario.getConstant(fieldName) + ": \"" + rowData.get(column) + "\"},\n"); // Ultima
-																													// coluna
+					jsCode.append(ColunasHorario.getConstant(fieldName) + ": \"" + rowData.get(column) + "\"},\n"); // Ultima coluna
 				else {
-					jsCode.append(ColunasHorario.getConstant(fieldName) + ": \"" + rowData.get(column) + "\","); // Resto
-																													// das
-																													// colunas
+					jsCode.append(ColunasHorario.getConstant(fieldName) + ": \"" + rowData.get(column) + "\","); // Resto das colunas
 					column++;
 				}
 			}
@@ -105,8 +101,8 @@ public class FileToTable {
 		jsCode.append("columns: [\n");
 
 		for (String column : this.mappedHeader)
-			jsCode.append("{ title: \"" + column + "\", field: \"" + ColunasHorario.getConstant(column)
-					+ "\", headerFilter: \"input\" },\n");
+			jsCode.append(
+					"{ title: \"" + column + "\", field: \"" + ColunasHorario.getConstant(column) + "\", headerFilter: \"input\" },\n");
 
 		jsCode.append("]\n");
 		jsCode.append("});\n");
@@ -120,37 +116,75 @@ public class FileToTable {
 					+ "		<script type=\"text/javascript\" src=\"https://unpkg.com/tabulator-tables@4.8.4/dist/js/tabulator.min.js\"></script>\r\n"
 					+ "	</head>");
 			writer.write("\n<body>\n\r" + "<H1>Horário</H1>	\r\n" + "<div>\r\n"
-					+ "    	<select id=\"filter-campo\" placeholder=\"campo\">\r\n" + "    	<option></option>\r\n"
+					+ "    	<select id=\"filter-campo\" placeholder=\"campo\">\r\n"
+					+ "    	<option></option>\r\n"
 					+ "    	<option value=\"InscritosNoTurno\">Inscritos no turno</option>\r\n"
 					+ "    	<option value=\"HoraInicioDaAula\">Hora inicio da aula</option>\r\n"
 					+ "    	<option value=\"HoraFimDaAula\">Hora fim da aula</option>\r\n"
-					+ "    	<option value=\"SalaAtribuídaÀAula\">Sala atribuida</option>\r\n" + "  		</select>\r\n"
-					+ "\r\n" + "  		<select id=\"filter-operador\">\r\n"
-					+ "		<option value=\"&\">&</option>\r\n" + "		<option value=\"+\">+</option>\r\n"
-					+ "		<option value=\"-\">-</option>\r\n" + "    	<option value=\"=\">=</option>\r\n"
-					+ "    	<option value=\"<\"><</option>\r\n" + "    	<option value=\"<=\"><=</option>\r\n"
-					+ "    	<option value=\">\">></option>\r\n" + "    	<option value=\">=\">>=</option>\r\n"
-					+ "    	<option value=\"!=\">!=</option>\r\n" + "    	<option value=\"like\">like</option>\r\n"
-					+ "  		</select>\r\n" + "  		\r\n"
-					+ "  		<select id=\"filter-campo\" placeholder=\"campo\">\r\n" + "    	<option></option>\r\n"
+					+ "    	<option value=\"SalaAtribuídaÀAula\">Sala atribuida</option>\r\n"
+					+ "    	<option value=\"UnidadeCurricular\">Unidade curricular</option>\r\n"
+					+ "    	<option value=\"Curso\">Curso</option>\r\n"
+					+ "    	<option value=\"Turno\">Turno</option>\r\n"
+					+ "    	<option value=\"Turma\">Turma</option>\r\n"
+					+ "    	<option value=\"DiaDaSemana\">Dia da semana</option>\r\n"
+					+ "    	<option value=\"DataDaAula\">Data da aula</option>\r\n"
+					+ "    	<option value=\"InscritosNoTurno\">Inscritos no turno</option>\r\n"
+					+ "    	<option value=\"CaracteristicasDaSalaPedidaParaAAula\">Caracteristicas da sala pedida para a aula</option>\r\n"
+					+ "  		</select>\r\n"
+					+ "\r\n"
+					+ "  		<select id=\"filter-operador\">\r\n"
+					+ "		<option value=\"&\">&</option>\r\n"
+					+ "		<option value=\"+\">+</option>\r\n"
+					+ "		<option value=\"-\">-</option>\r\n"
+					+ "    	<option value=\"=\">=</option>\r\n"
+					+ "    	<option value=\"<\"><</option>\r\n"
+					+ "    	<option value=\"<=\"><=</option>\r\n"
+					+ "    	<option value=\">\">></option>\r\n"
+					+ "    	<option value=\">=\">>=</option>\r\n"
+					+ "    	<option value=\"!=\">!=</option>\r\n"
+					+ "    	<option value=\"like\">like</option>\r\n"
+					+ "  		</select>\r\n"
+					+ "  		\r\n"
+					+ "  		<select id=\"filter-campo\" placeholder=\"campo\">\r\n"
+					+ "    	<option></option>\r\n"
 					+ "    	<option value=\"InscritosNoTurno\">Inscritos no turno</option>\r\n"
 					+ "    	<option value=\"HoraInicioDaAula\">Hora inicio da aula</option>\r\n"
 					+ "    	<option value=\"HoraFimDaAula\">Hora fim da aula</option>\r\n"
-					+ "    	<option value=\"SalaAtribuídaÀAula\">Sala atribuida</option>\r\n" + "  		</select>\r\n"
-					+ "  		\r\n" + "  		<select id=\"filter-operador\">\r\n"
-					+ "		<option value=\"&\">&</option>\r\n" + "		<option value=\"+\">+</option>\r\n"
-					+ "		<option value=\"-\">-</option>\r\n" + "    	<option value=\"=\">=</option>\r\n"
-					+ "    	<option value=\"<\"><</option>\r\n" + "    	<option value=\"<=\"><=</option>\r\n"
-					+ "    	<option value=\">\">></option>\r\n" + "    	<option value=\">=\">>=</option>\r\n"
-					+ "    	<option value=\"!=\">!=</option>\r\n" + "    	<option value=\"like\">like</option>\r\n"
-					+ "  		</select>\r\n" + "\r\n"
-					+ "  		<input id=\"filter-valor\" type=\"text\" placeholder=\"valor\">\r\n" + "  		\r\n"
-					+ "  		<button id=\"filter-resultado\">Obter resultado</button>\r\n" + "\r\n"
-					+ "  		<button id=\"filter-clear\">Clear Filter</button>\r\n" + "	</div>"
-					+ "<div id=\"resultado-obtido\">\r\n" + "  		<h2>Resultado</h2>\r\n"
+					+ "    	<option value=\"SalaAtribuídaÀAula\">Sala atribuida</option>\r\n"
+					+ "    	<option value=\"UnidadeCurricular\">Unidade curricular</option>\r\n"
+					+ "    	<option value=\"Curso\">Curso</option>\r\n"
+					+ "    	<option value=\"Turno\">Turno</option>\r\n"
+					+ "    	<option value=\"Turma\">Turma</option>\r\n"
+					+ "    	<option value=\"DiaDaSemana\">Dia da semana</option>\r\n"
+					+ "    	<option value=\"DataDaAula\">Data da aula</option>\r\n"
+					+ "    	<option value=\"InscritosNoTurno\">Inscritos no turno</option>\r\n"
+					+ "    	<option value=\"CaracteristicasDaSalaPedidaParaAAula\">Caracteristicas da sala pedida para a aula</option>\r\n"
+					+ "  		</select>\r\n"
+					+ "  		\r\n"
+					+ "  		<select id=\"filter-operador\">\r\n"
+					+ "		<option value=\"&\">&</option>\r\n"
+					+ "		<option value=\"+\">+</option>\r\n"
+					+ "		<option value=\"-\">-</option>\r\n"
+					+ "    	<option value=\"=\">=</option>\r\n"
+					+ "    	<option value=\"<\"><</option>\r\n"
+					+ "    	<option value=\"<=\"><=</option>\r\n"
+					+ "    	<option value=\">\">></option>\r\n"
+					+ "    	<option value=\">=\">>=</option>\r\n"
+					+ "    	<option value=\"!=\">!=</option>\r\n"
+					+ "    	<option value=\"like\">like</option>\r\n"
+					+ "  		</select>\r\n"
+					+ "\r\n"
+					+ "  		<input id=\"filter-valor\" type=\"text\" placeholder=\"valor\">\r\n"
+					+ "  		\r\n"
+					+ "  		<button id=\"filter-resultado\">Obter resultado</button>\r\n"
+					+ "\r\n"
+					+ "  		<button id=\"filter-clear\">Clear Filter</button>\r\n"
+					+ "	</div>" + "<div id=\"resultado-obtido\">\r\n"
+					+ "  		<h2>Resultado</h2>\r\n"
 					+ "  		<div id=\"filter-result\">\r\n"
-					+ "    		<!-- Filter results will be displayed here -->\r\n" + "  		</div>\r\n" + "	</div>"
-					+ "<div id=\"example-table\"></div>\n\n");
+					+ "    		<!-- Filter results will be displayed here -->\r\n"
+					+ "  		</div>\r\n"
+					+ "	</div>" + "<div id=\"example-table\"></div>\n\n");
 			writer.write(jsCode.toString());
 			writer.write("</body>\n</html>");
 		} catch (IOException e) {
@@ -159,10 +193,10 @@ public class FileToTable {
 	}
 
 	/**
-	 * Checks if the CSV file has a header containing expected column names.
-	 *
-	 * @return `true` if a matching header is found, `false` otherwise.
-	 */
+     * Checks if the CSV file has a header containing expected column names.
+     *
+     * @return `true` if a matching header is found, `false` otherwise.
+     */
 	private boolean hasHeader() {
 		List<String> colunasHorario = ColunasHorario.valuesList();
 		try (CSVParser parser = CSVParser.parse(new FileReader(file), CSVFormat.EXCEL.withDelimiter(';'))) {
@@ -179,47 +213,46 @@ public class FileToTable {
 	}
 
 	/**
-	 * Checks if the CSV columns are automatically mapped.
-	 *
-	 * @return `true` if columns are automatically mapped, `false` otherwise.
-	 */
-	public boolean isColumnsMapped() {
-		return columnsMapped;
-	}
+     * Checks if the CSV columns are automatically mapped.
+     *
+     * @return `true` if columns are automatically mapped, `false` otherwise.
+     */
+    public boolean isColumnsMapped() {
+        return columnsMapped;
+    }
 
-	/**
-	 * Sets whether the CSV columns are automatically mapped.
-	 *
-	 * @param autoMapped `true` to indicate automatic mapping, `false` otherwise.
-	 */
-	public void setColumnsMapped(boolean autoMapped) {
-		this.columnsMapped = autoMapped;
-	}
+    /**
+     * Sets whether the CSV columns are automatically mapped.
+     *
+     * @param autoMapped `true` to indicate automatic mapping, `false` otherwise.
+     */
+    public void setColumnsMapped(boolean autoMapped) {
+        this.columnsMapped = autoMapped;
+    }
 
-	/**
-	 * Gets the header data extracted from the CSV file.
-	 *
-	 * @return A list of header column names. List will be empty if the file has no
-	 *         header.
-	 */
-	public List<String> getMappedHeader() {
-		return mappedHeader;
-	}
+    /**
+     * Gets the header data extracted from the CSV file.
+     *
+     * @return A list of header column names. List will be empty if the file has no header.
+     */
+    public List<String> getMappedHeader() {
+        return mappedHeader;
+    }
 
-	/**
-	 * Sets the header data extracted from the CSV file.
-	 *
-	 * @param header A list of header column names.
-	 */
-	public void setMappedHeader(List<String> header) {
-		this.mappedHeader = header;
-	}
+    /**
+     * Sets the header data extracted from the CSV file.
+     *
+     * @param header A list of header column names.
+     */
+    public void setMappedHeader(List<String> header) {
+        this.mappedHeader = header;
+    }
 
-	/**
-	 * The main method for testing `FileToTable` class functionality.
-	 *
-	 * @param args The command-line arguments.
-	 */
+    /**
+     * The main method for testing `FileToTable` class functionality.
+     *
+     * @param args The command-line arguments.
+     */
 	public static void main(String[] args) {
 		File file = new File("C:\\Users\\leoth\\Downloads\\HorarioDeExemplo.csv");
 		FileToTable ftt = new FileToTable(file);
